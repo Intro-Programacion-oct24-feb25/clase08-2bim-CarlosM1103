66f2ec96bb14eac51b4d3145092cf254a91ab757
CarlosM1103 <carlosmedinal2006@gmail.com>
2025-01-29 11:53:43 -0500

    hecho

--- Ejercicios07/src/ejemplo002/CrearArchivoTexto.java
+++ Ejercicios07/src/ejemplo002/CrearArchivoTexto.java
@@ -28,11 +28,11 @@ public class CrearArchivoTexto {
  *                                                                        *
  * RENUNCIA: Los autores y el editor de este libro han realizado su mejor *
  * esfuerzo para preparar este libro. Esto incluye el desarrollo, la      *
- * investigaci�n y prueba de las teor�as y programas para determinar su   *
+ * investigaci�n y prueba de las teor�as y programas para determinar su  *
  * efectividad. Los autores y el editor no hacen ninguna garant�a de      *
- * ning�n tipo, expresa o impl�cita, en relaci�n con estos programas o    *
- * con la documentaci�n contenida en estos libros. Los autores y el       *
- * editor no ser�n responsables en ning�n caso por los da�os consecuentes *
+ * ning�n tipo, expresa o impl�cita, en relaci�n con estos programas o   *
+ * con la documentaci�n contenida en estos libros. Los autores y el       * 
+ * editor no ser�n responsables en ning�n caso por los da�os consecuentes*
  * en conexi�n con, o que surjan de, el suministro, desempe�o o uso de    *
- * estos programas.                                                       *
+ * estos programas.                                                        *
  *************************************************************************/
